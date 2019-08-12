7576a9b6b4ed9c7f13904a9dcb9ae1d586e851f5
Adam Knowles <acknowles524@gmail.com>
2019-08-12 03:17:57 -0500

    got delete and edit working for procedure notes

---
+++ src/components/UserProfile/NoteProcedureEditFormModal.js
@@ -0,0 +1,123 @@
+import React, { Component } from 'react'
+import { Card, CardBody, Button,Modal, ModalFooter } from "reactstrap";
+import APImanager from "../../modules/APImanager"
+
+export default class NoteProcedureEditFormModal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          userId: sessionStorage.getItem("userId"),
+          modal: false,
+          id: "",
+          note: "",
+          procedureId: ""
+          
+        };
+    
+        this.toggle = this.toggle.bind(this);
+      
+      }
+    
+      toggle() {
+        this.setState(prevState => ({
+          modal: !prevState.modal,
+           
+        }));
+      }
+
+      handleFieldChange = (evt) => {
+        const stateToChange = {};
+        stateToChange[evt.target.id] = evt.target.value;
+        this.setState(stateToChange);
+        
+      }
+      editNote = evt => {
+        evt.preventDefault();
+        const note = {
+          userId: this.state.userId,
+          procedureId:this.props.note.procedureId,
+          note: this.state.note,
+          id: this.props.note.id
+        };
+        
+    
+        this.props.editNewNote(note)
+        this.state.modal=false
+    }
+    componentDidMount() {
+        
+        APImanager.get("notes", this.props.note.id).then(note => {
+          this.setState({
+            id:note.id,
+            note:note.note
+            
+          });
+        });
+      }
+    
+    
+    render() {
+        
+        return (
+            <React.Fragment>
+            <div>
+                <Button className="edit" color="primary" size=""  onClick={this.toggle}>Edit</Button>
+            </div>
+            <div>
+                <Modal
+                isOpen={this.state.modal}
+                toggle={this.toggle}
+                className={this.props.className} size="lg"
+              >
+                {/* <ModalHeader toggle={this.toggle}>Modal title</ModalHeader> */}
+                <Card
+                  body
+                  className="text-center"
+                  body
+                  inverse
+                  style={{
+                    backgroundColor: "#93E9BE",
+                    borderColor: "#93E9BE",
+                    color: "#93E9BE"
+                  }}
+                >
+                  <CardBody className="text-dark">
+                      <h2 className="p-1 mb-4 bg-secondary text-white">Edit Note </h2>
+                  <h1>
+                      <u>{this.props.procedure.name}</u>
+                    </h1>
+                    
+                    
+                    <textarea
+              onChange={this.handleFieldChange}
+              type="text"
+              id="note"
+              value={this.state.note}
+              required
+              autoFocus=""
+              className="form-control mb-2"
+              rows="8"
+            />
+                  </CardBody>
+                </Card>
+
+                <ModalFooter>
+                  <Button
+                    color="secondary"
+                    onClick={this.toggle} onClick={this.editNote}
+                    
+                    
+                  >
+                    Save 
+                  </Button>
+                  <Button color="secondary" onClick={this.toggle}>
+                    Cancel
+                  </Button>
+                </ModalFooter>
+              </Modal>
+                
+            </div>
+            </React.Fragment>
+        )
+    }
+}
--- src/components/UserProfile/addNoteProcedureToProfile.js
+++ src/components/UserProfile/addNoteProcedureToProfile.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Card, CardBody, CardFooter, Button,Modal, ModalFooter } from "reactstrap";
 import AddNoteProcedureModal from "./AddProcedureNoteModal"
+import NoteProcedureEditFormModal from "./NoteProcedureEditFormModal"
 
 
 
@@ -39,6 +40,7 @@ export default class AddNoteProcedureToProfile extends Component {
         <Modal isOpen={this.state.modal}
                 toggle={this.toggle}
                 className={this.props.className} size="lg">
+                    <div key={this.props.notes.id} ></div>
                   
           <Card
               
@@ -55,6 +57,17 @@ export default class AddNoteProcedureToProfile extends Component {
              
                 
                 <AddNoteProcedureModal procedures={this.props.procedures} procedure={this.procedure} {...this.props} />
+                <div>
+                {this.props.notes.filter(
+                note =>
+                  note.procedureId === this.props.procedure.procedureId).
+              map( note =>(<React.Fragment><CardBody className="text-dark mt-4" 
+              inverse
+              style={{ backgroundColor: "#93E9BE", borderColor: "#FFF4EF" }}><div ><CardBody className="text-dark border border-dark" 
+              inverse
+              style={{ backgroundColor: "#FFF4EF", borderColor: "#FFF4EF" }}><h6 className="mt-0">{note.note}</h6></CardBody><div className="editDeleteButtons" > <NoteProcedureEditFormModal toggle={this.toggle} editNewNote={this.props.editNewNote} notes={this.props.notes} note={note} {...this.props} />  <Button color="danger" size="" onClick={() =>this.props.deleteNoteFromProfile(note.id)} >
+                    Delete
+                  </Button></div></div></CardBody></React.Fragment>))}</div>
                    
                   <Button  onClick={this.toggle} className=" btn mt-4">
                     Cancel
